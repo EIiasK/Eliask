ca96c87786e7168a8d7e5afe39aa7cf27a890b35
Windose <2634371141@qq.com>
2024-09-24 16:04:16 +0900

    解决真分数的显示问题，将分数转化为字符串处理

--- 3122004566/Exercise_Generator/Myapp.py
+++ 3122004566/Exercise_Generator/Myapp.py
@@ -4,3 +4,18 @@ import random    # 用于生成随机数
 import sys       # 用于退出程序
 from fractions import Fraction  # 用于处理分数
 
+def number_to_string(number):
+    """
+    将 Fraction 类型的数转换为字符串表示形式，支持真分数和带分数。
+    """
+    if number.denominator == 1:
+        # 如果是整数
+        return str(number.numerator)
+    elif number.numerator > number.denominator:
+        # 如果是带分数
+        whole = number.numerator // number.denominator  # 整数部分
+        remainder = number.numerator % number.denominator  # 分数部分的分子
+        return f"{whole}'{remainder}/{number.denominator}"
+    else:
+        # 如果是真分数
+        return f"{number.numerator}/{number.denominator}"
